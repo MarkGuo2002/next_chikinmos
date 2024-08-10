9843d5836a809731f6292c46e851d77314e1dced
MarkGuo2002 <100450691@alumnos.uc3m.es>
2024-08-10 22:54:31 +0200

    hello world done for chikinmos

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div className="bg-gray-800 text-center">
+            <h1>Hello, world!</h1>
+            <p>Welcome to your new Next.js app.</p>
+        </div>
+    );
+    }
